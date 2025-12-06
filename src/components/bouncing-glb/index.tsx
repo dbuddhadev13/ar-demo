78f5cd76a022b80981f82749f4df667ad8406716
Dhyey Buddhadev <dhyey@intellimedianetworks.net>
2025-12-06 14:30:44 +0100

    optimized models

--- src/components/bouncing-glb/index.tsx
+++ src/components/bouncing-glb/index.tsx
@@ -1,4 +1,3 @@
-import { Gltf } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { BallCollider, RapierRigidBody, RigidBody } from '@react-three/rapier';
 import { ReactNode, RefObject, useRef, useState } from 'react';
